e622db5966975a8c43bb7d72bf0396ac480388d6
Pedro Carvalho <85134350+Pedrohcbx@users.noreply.github.com>
2023-10-02 16:57:08 -0300

    ....

---
+++ src/paginas/Home.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+function Home() {
+
+    return (
+        <div>
+            <h1>Joško Gvardiol</h1>
+            <Link to='/joao'>Ver a página do João</Link>
+        </div>
+    )
+  }
+  
+  export default Home
+  
---
+++ src/paginas/Joao.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+function Joao(){
+    return(
+        <div>
+            <h1>O João é um zé ninguém</h1>
+        <Link to='/'>Ver a página Inicial</Link>
+        </div>
+    )
+}
+
+export default Joao
